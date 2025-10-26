1b5624bab3c70be7fe81b25383471de4e316a2a5
filipemartinsdev <dev.filipemartins@gmail.com>
2025-10-25 20:50:05 -0400

    feat: initialize server in App

--- src/main/java/com/libraryapi/App.java
+++ src/main/java/com/libraryapi/App.java
@@ -4,8 +4,28 @@ import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpExchange;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
     public static void main(String[] args) {
+        final String PORT = System.getenv("PORT");
+        HttpServer server = null;
 
+        try {
+            server = HttpServer.create(
+                    new InetSocketAddress("0.0.0.0", Integer.parseInt(PORT)), 0
+            );
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        finally {
+            if (server!=null){
+                server.start();
+                System.out.println("[OK] Server ON");
+            }
+        }
     }
 }
