8e8bcf69519b9f9370bb82c3fffa6f388e845e31
filipemartinsdev <dev.filipemartins@gmail.com>
2025-10-25 20:40:04 -0400

    build: create default environment

---
+++ src/main/java/com/libraryapi/App.java
@@ -0,0 +1,11 @@
+package com.libraryapi;
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
