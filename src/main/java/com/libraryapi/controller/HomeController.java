d682ed1394cbcf895073170bca5e782827713cba
filipemartinsdev <dev.filipemartins@gmail.com>
2025-10-25 21:03:06 -0400

    feat(HomeController): add standard Routes

--- src/main/java/com/libraryapi/controller/HomeController.java
+++ src/main/java/com/libraryapi/controller/HomeController.java
@@ -8,6 +8,17 @@ import java.io.IOException;
 public class HomeController implements HttpHandler {
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-
+        switch (httpExchange.getRequestMethod()){
+            case "GET" -> processGet(httpExchange);
+            case "POST" -> processPost(httpExchange);
+            default: processDefault(httpExchange);
+        }
     }
+
+    private void processGet(HttpExchange httpExchange){}
+
+    private void processPost(HttpExchange httpExchange){}
+
+    private void processDefault(HttpExchange httpExchange){}
+
 }
